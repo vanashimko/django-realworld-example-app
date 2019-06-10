296a8c0d4099f5a2d861c15a82044108b3822793
Ivan Shimko <vana.shimko@gmail.com>
2019-06-10 21:53:36 +0300

    Add tests, remove boilerplate, update request schema

--- conduit/apps/articles/tests.py
+++ conduit/apps/articles/tests.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from model_mommy import mommy
 
-from conduit.apps.articles.models import Article
+from conduit.apps.articles.models import Article, Tag
 from conduit.apps.authentication.models import User
 
 
@@ -16,14 +16,12 @@ class ArticlesTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="{0} {1}".format('Token', self.test_user.token))
 
     def test_should_create_article(self):
-        article_payload = {
+        expected_article = {
             'title': 'title',
             'body': 'body',
             'author': self.test_user.id
         }
-        response = self.client.post(reverse('articles:articles-list'), data={
-            'article': article_payload
-        })
+        response = self.client.post(reverse('articles:articles-list'), data=expected_article)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response.data)
         from_db = Article.objects.get(slug=response.data['slug'])
@@ -32,7 +30,24 @@ class ArticlesTests(APITestCase):
             'body': from_db.body,
             'author': self.test_user.id
         }
-        self.assertEqual(actual, article_payload)
+        self.assertEqual(actual, expected_article)
+
+    def test_should_create_article_based_on_authorized_user(self):
+        expected_article = {
+            'title': 'title',
+            'body': 'body',
+        }
+        response = self.client.post(reverse('articles:articles-list'), data=expected_article)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response.data)
+        from_db = Article.objects.get(slug=response.data['slug'])
+        actual = {
+            'title': from_db.title,
+            'body': from_db.body,
+            'author': from_db.author.id
+        }
+        expected_article['author'] = self.test_user.id
+        self.assertEqual(actual, expected_article)
 
     def test_should_retrieve_all_articles(self):
         quantity = 3
@@ -51,3 +66,46 @@ class ArticlesTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], article.title)
+
+    def test_should_update_single_article(self):
+        original_article = mommy.make(Article, slug=None, author=self.test_user.profile)
+        another_user = User.objects.create_user('new_name', 'a@b.com')
+        article_payload = {
+            'title': 'title',
+            'body': 'body',
+            'author': another_user.id
+        }
+
+        response = self.client.put(reverse('articles:articles-detail', kwargs={'slug': original_article.slug}),
+                                   data=article_payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        from_db = Article.objects.get(slug=original_article.slug)
+        actual = {
+            'title': from_db.title,
+            'body': from_db.body,
+            'author': another_user.id
+        }
+        self.assertEqual(actual, article_payload)
+
+    def test_should_retrieve_article_by_tag(self):
+        tag1 = Tag.objects.create(tag='tag1', slug='tag1')
+        tag2 = Tag.objects.create(tag='tag2', slug='tag2')
+        expected_article = mommy.make(Article, slug=None, tags=[tag1], author=self.test_user.profile)
+        mommy.make(Article, slug=None, tags=[tag2], author=self.test_user.profile)
+
+        response = self.client.get(reverse('articles:articles-list'), {'tag': tag1.tag})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['slug'], expected_article.slug)
+
+    def test_should_retrieve_article_by_author(self):
+        another_user = User.objects.create_user('new_name', 'a@b.com')
+        expected_article = mommy.make(Article, slug=None, author=another_user.profile)
+        mommy.make(Article, slug=None, author=self.test_user.profile)
+
+        response = self.client.get(reverse('articles:articles-list'), {'author': another_user.username})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['slug'], expected_article.slug)
--- conduit/apps/articles/views.py
+++ conduit/apps/articles/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins, status, viewsets
+from rest_framework import generics, status, viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import (
     AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
@@ -11,11 +11,7 @@ from .renderers import ArticleJSONRenderer, CommentJSONRenderer
 from .serializers import ArticleSerializer, CommentSerializer, TagSerializer
 
 
-class ArticleViewSet(mixins.CreateModelMixin,
-                     mixins.ListModelMixin,
-                     mixins.RetrieveModelMixin,
-                     viewsets.GenericViewSet):
-
+class ArticleViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     lookup_value_regex = r'[a-z0-9\-]+'
     queryset = Article.objects.select_related('author', 'author__user')
@@ -44,67 +40,16 @@ class ArticleViewSet(mixins.CreateModelMixin,
 
     def create(self, request):
         serializer_context = {
-            'author': request.user.profile,
-            'request': request
+            'author': request.user.profile
         }
-        serializer_data = request.data.get('article', {})
 
         serializer = self.serializer_class(
-        data=serializer_data, context=serializer_context
+            data=request.data, context=serializer_context
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def list(self, request):
-        serializer_context = {'request': request}
-        page = self.paginate_queryset(self.get_queryset())
-
-        serializer = self.serializer_class(
-            page,
-            context=serializer_context,
-            many=True
-        )
-
-        return self.get_paginated_response(serializer.data)
-
-    def retrieve(self, request, slug):
-        serializer_context = {'request': request}
-
-        try:
-            serializer_instance = self.queryset.get(slug=slug)
-        except Article.DoesNotExist:
-            raise NotFound('An article with this slug does not exist.')
-
-        serializer = self.serializer_class(
-            serializer_instance,
-            context=serializer_context
-        )
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-    def update(self, request, slug):
-        serializer_context = {'request': request}
-
-        try:
-            serializer_instance = self.queryset.get(slug=slug)
-        except Article.DoesNotExist:
-            raise NotFound('An article with this slug does not exist.')
-            
-        serializer_data = request.data.get('article', {})
-
-        serializer = self.serializer_class(
-            serializer_instance, 
-            context=serializer_context,
-            data=serializer_data, 
-            partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CommentsListCreateAPIView(generics.ListCreateAPIView):
